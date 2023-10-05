ba9d66d02448a405d3452b4a1e8770dd05434d7f
Kawin Viriyaprasopsook <kawin.v@kkumail.com>
2023-10-05 22:30:57 +0700

    📝 story explain

--- basic-number-filtering/story5/main.go
+++ basic-number-filtering/story5/main.go
@@ -9,17 +9,35 @@ import (
 )
 
 func main() {
+	// Prompt the user for input
 	fmt.Print("Sample Input: ")
+
+	// Create a new scanner to read user input from the standard input
 	input := bufio.NewScanner(os.Stdin)
+
+	// Read the user's input
 	input.Scan()
 
+	// Extract numbers from the user input
 	numbers := utils.ExtractNumbers(input.Text())
+
+	// Filter the numbers to keep only the even ones
 	result := utils.Filter(numbers, utils.IsEven)
+
+	// Filter the result again to keep only the numbers that are multiples of five
 	result = utils.Filter(result, IsMultipleByFive)
 
+	// Print the sample output
 	fmt.Printf("Sample Output: %s\n", utils.OutputString(result))
 }
 
+// IsMultipleByFive checks if the given number is a multiple of five.
+//
+// Parameters:
+//   number - an integer to check
+//
+// Returns:
+//   bool - true if the number is a multiple of five, false otherwise.
 func IsMultipleByFive(number int) bool {
 	return number%5 == 0
 }
--- basic-number-filtering/story6/main.go
+++ basic-number-filtering/story6/main.go
@@ -9,22 +9,46 @@ import (
 )
 
 func main() {
+	// Prompt the user for input
 	fmt.Print("Sample Input: ")
+
+	// Read the input from the user
 	input := bufio.NewScanner(os.Stdin)
+
+	// Read the user's input
 	input.Scan()
 
+	// Extract the numbers from the input
 	numbers := utils.ExtractNumbers(input.Text())
+
+	// Filter out odd numbers
 	result := utils.Filter(numbers, utils.IsOdd)
+
+	// Filter out numbers that are not multiples of three
 	result = utils.Filter(result, IsMultipleByThree)
+
+	// Filter out numbers that are not greater than ten
 	result = utils.Filter(result, IsGreatherThanTen)
 
+	// Print the output
 	fmt.Printf("Sample Output: %s\n", utils.OutputString(result))
 }
 
+// IsMultipleByThree checks if a given number is a multiple of three.
+//
+// Parameters:
+// - number: an integer number to be checked.
+//
+// Returns:
+// - a boolean value indicating whether the number is a multiple of three.
 func IsMultipleByThree(number int) bool {
 	return number%3 == 0
 }
 
+// IsGreatherThanTen checks if the given number is greater than ten.
+//
+// number: an integer to be checked.
+// returns: a boolean indicating if the number is greater than ten.
 func IsGreatherThanTen(number int) bool {
 	return number > 10
 }
